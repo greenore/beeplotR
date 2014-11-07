1ce77222442aa6102d4bc82bf155fe0d55c5e50b
greenore <tim.hagmann@gmail.com>
2014-11-07 14:37:31 +0100

    update function

--- R/beeplot.R
+++ R/beeplot.R
@@ -5,7 +5,7 @@
 #'   
 #' @param id
 #' @param category
-#' @param praemie
+#' @param num_var
 #' @param rank
 #' @param data
 #' @param color
@@ -26,98 +26,94 @@
 #' @param las
 #'
 
-beePlot <- function(id, category, praemie, rank, data, color, range = c(0, max),
-                    conf = T, median = T, mean = F, label = T, bee_plot = T,
-                    cex = 6, cex.axis = 1.8, cex.lab = 1.4, cex.med = 2.2, 
-                    cex.minmax = 1.3, lwd.med = 3, las = 1, method = 'center', corral = "random"){
+beePlot <- function(id, category, num_var, rank, data, color, range=c(0, max),
+                    conf=TRUE, median=TRUE, mean=FALSE, label=TRUE, bee_plot=TRUE,
+                    cex=6, cex.axis=1.8, cex.lab=1.4, cex.med=2.2,
+                    cex.minmax=1.3, lwd.med=3, las=1, method='center', corral="random"){
   require(visualizeR)
   
   # Ceiling
-  data[, praemie] <- as.numeric(data[, praemie])
-  df <- data[!is.na(data[, praemie]), ]
+  data[, num_var] <- as.numeric(data[, num_var])
+  df <- data[!is.na(data[, num_var]), ]
+  max <- ceiling(max(df[, num_var])/100)*100
   
-  max <- ceiling(max(df[, praemie])/100)*100
+  # Number of Top 3
+  n_top_3 <- table(data[, rank], data[, category])
+  n_top_3 <- colSums(n_top_3[1:3, ])/colSums(n_top_3)
+  n_top_3 <- n_top_3[order(n_top_3, decreasing=TRUE)]
+  
+  # Order according to Top 3
+  df[, category] <- factor(df[, category], levels=names(n_top_3))
   
   # Aggregation
-  aggPrime <- aggFun(praemie = praemie, category = category, data = df)
-  aggRank <- aggFun(praemie = rank, category = category, data = df)
-  names(aggRank)[names(aggRank) == 'Mean'] <- 'averageMean'
-  
-  # Merge
-  df <- merge(df, aggRank[, c(category, 'averageMean')], by = category)
-  aggPrime <- merge(aggPrime, aggRank[, c(category, 'averageMean')], by = category)
-  
-  # Order according to mean rank
-  aggPrime[, category] <- reorder(aggPrime[, category], aggPrime$averageMean, FUN = 'mean')
-  aggPrime <- aggPrime[order(aggPrime$averageMean), ]
-  df[, category] <- reorder(df[, category], df$averageMean, FUN = 'mean')
+  aggPrime <- aggFun(praemie=num_var, category=category, data=df)
   
   # Plotting
-  par(mar = c(5, 6, 4, 2) + 0.1)
+  par(mar=c(5, 6, 4, 2) + 0.1)
   
-  if (bee_plot == F){
-    boxplot(eval(parse(text = textFun(praemie, category))), data = df, border = F,
-            cex = cex, las = las, cex.axis = cex.axis, ylim = range)
+  if (bee_plot == FALSE){
+    boxplot(eval(parse(text=textFun(num_var, category))), data=df, border=FALSE,
+            cex=cex, las=las, cex.axis=cex.axis, ylim=range)
   }
   
   # Beeplot
-  if (bee_plot == T){
+  if (bee_plot){
     col <- as.character(df[, color])
     
     set.seed(123)
-    bee <- beeswarm(eval(parse(text = textFun(praemie, category))), data = df, 
-                    vertical = T, spacing = 0.7, cex = cex,
-                    pwcol = col, pch = 18,
-                    xlab = '', ylab = '', main = '',
-                    ylim = range, las = las, cex.axis = cex.axis,
-                    pwbg = as.character(df[, id]), 
-                    method = method, corral = corral)
+    bee <- beeswarm(eval(parse(text=textFun(num_var, category))), data=df,
+                    vertical=TRUE, spacing=0.7, cex=cex,
+                    pwcol=col, pch=18,
+                    xlab='', ylab='', main='',
+                    ylim=range, las=las, cex.axis=cex.axis,
+                    pwbg=as.character(df[, id]),
+                    method=method, corral=corral)
     
     # Label: Numbers
-    if (label == T){
-      text(bee, labels = bee$bg, cex = cex.lab, col = 'white', font = 2)
+    if (label){
+      text(bee, labels=bee$bg, cex=cex.lab, col='white', font=2)
     }
     
     # Line: Median
-    if (median == T){
-      boxplot(eval(parse(text = textFun('Median', category))), add = T, data = aggPrime,
-              xlab = "", ylab = "", axes = F, border = 'black', boxwex = .8, lwd = lwd.med, ylim = range)
+    if (median){
+      boxplot(eval(parse(text=textFun('Median', category))), add=TRUE, data=aggPrime,
+              xlab="", ylab="", axes=F, border='black', boxwex=.8, lwd=lwd.med, ylim=range)
     }
     
-    if (mean == T){
-      boxplot(eval(parse(text = textFun('Mean', category))), add = T, data = aggPrime,
-              xlab = "", ylab = "", axes = F, border = balCol('rot'), boxwex = .8, lwd = lwd.med, ylim = range)
+    if (mean){
+      boxplot(eval(parse(text=textFun('Mean', category))), add=T, data=aggPrime,
+              xlab="", ylab="", axes=F, border=balCol('rot'), boxwex=.8, lwd=lwd.med, ylim=range)
     }
     
     # Label: Median
-    if (median == T){
-      lab <- paste('Median', ' = ', sep = '')
+    if (median){
+      lab <- paste('Median', '=', sep='')
       text(rep(0.5 * range[2]/100, length(levels(df[, category]))),
-           labels = paste(lab, round(aggPrime$Median, 0), '.-', sep = ''),
-           cex = cex.med, col = 'black', font = 2)
+           labels=paste(lab, round(aggPrime$Median, 0), '.-', sep=''),
+           cex=cex.med, col='black', font=2)
     }
     
     # Conf Intervall
-    col <- ifelse(conf, rgb(0/255, 51/255, 153/255, alpha = 0.3), NA)
-    box <- boxplot(eval(parse(text = textFun(praemie, category))), data = df, plot = F)
+    col <- ifelse(conf, rgb(0/255, 51/255, 153/255, alpha=0.3), NA)
+    box <- boxplot(eval(parse(text=textFun(num_var, category))), data=df, plot=FALSE)
     rect((1:length(levels(df[, category]))) - 0.45, box$conf[1, ],
          (1:length(levels(df[, category]))) + 0.45, box$conf[2, ],
-         col = col, border = NA)
+         col=col, border=NA)
     
     # Label: CI_L & CI_U
-    if (conf == T){
+    if (conf){
       text(rep(-2.5 * range[2]/100, length(levels(df[, category]))),
-           labels = paste('95% CI: [', round(box$conf[1, ], 0), '.- ; ',
-                          round(box$conf[2, ], 0), '.-]', sep = ''),
-           cex = cex.minmax, col = 'black', font = 2)
-    } 
+           labels=paste('95% CI: [', round(box$conf[1, ], 0), '.- ; ',
+                        round(box$conf[2, ], 0), '.-]', sep=''),
+           cex=cex.minmax, col='black', font=2)
+    }
     
     # Label: Min Max
-    if (conf == F & median == T){
+    if (conf == FALSE & median){
       text(rep(-2.5 * range[2]/100, length(levels(df[, category]))),
-           labels = paste('(Min = ', round(aggPrime$Min, 0), '.-',
-                          '  Max = ', round(aggPrime$Max, 0), '.-)', sep = ''),
-           cex = cex.minmax, col = 'black', font = 2)
-    } 
+           labels=paste('(Min = ', round(aggPrime$Min, 0), '.-',
+                        '  Max = ', round(aggPrime$Max, 0), '.-)', sep=''),
+           cex=cex.minmax, col='black', font=2)
+    }
   }
 }
